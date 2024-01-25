adc33e3240495206af78ea4543d8c72a88f8c6ff
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-01-25 19:53:37 +0530

    Added and modified files

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,8 +1,5 @@
 const CDN_URL = import.meta.env.VITE_CDN_URL;
 const LOGO_URL = import.meta.env.VITE_LOGO_URL;
-
 const swiggy_api_URL = import.meta.env.VITE_SWIGGY_API_URL;
-
-const MENU_API = import.meta.env.MENU_API;
-
+const MENU_API = import.meta.env.VITE_MENU_API;
 export { CDN_URL, LOGO_URL, swiggy_api_URL, MENU_API };
