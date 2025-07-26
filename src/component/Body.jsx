b1db1c7b09d6fa6d8b62720a7b124957f0e92ba7
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2025-07-26 11:12:55 +0530

    update the food api fix cors issue

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -7,132 +7,6 @@ import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "./UserContext";
 
 const Body = () => {
-  // let listOfRestaurantsJS = [
-  //   {
-  //     card: {
-  //       card: {
-  //         restaurant: {
-  //           info: {
-  //             id: "80278",
-  //             name: "Gaagar New Market",
-  //             city: "55",
-  //             slugs: {
-  //               restaurant: "gaagar-tt-nagar-new-market",
-  //               city: "bhopal",
-  //             },
-  //             cloudinaryImageId: "nhaax0gshegi6khog5th",
-  //             address: "32/42, New Market, TT Nagar, Bhopal",
-  //             areaName: "New Market",
-  //             costForTwo: "30000",
-  //             costForTwoMessage: "₹300 FOR TWO",
-  //             cuisines: [
-  //               "Sweets",
-  //               "Beverages",
-  //               "Desserts",
-  //               "Chaat",
-  //               "South Indian",
-  //               "North Indian",
-  //             ],
-  //             avgRating: 4.3,
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  //   {
-  //     card: {
-  //       card: {
-  //         restaurant: {
-  //           info: {
-  //             id: "80278",
-  //             name: "Gaagar New Market",
-  //             city: "55",
-  //             slugs: {
-  //               restaurant: "gaagar-tt-nagar-new-market",
-  //               city: "bhopal",
-  //             },
-  //             cloudinaryImageId: "nhaax0gshegi6khog5th",
-  //             address: "32/42, New Market, TT Nagar, Bhopal",
-  //             areaName: "New Market",
-  //             costForTwo: "30000",
-  //             costForTwoMessage: "₹300 FOR TWO",
-  //             cuisines: [
-  //               "Sweets",
-  //               "Beverages",
-  //               "Desserts",
-  //               "Chaat",
-  //               "South Indian",
-  //               "North Indian",
-  //             ],
-  //             avgRating: 4.3,
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  //   {
-  //     card: {
-  //       card: {
-  //         restaurant: {
-  //           info: {
-  //             id: "80278",
-  //             name: "Gaagar New Market",
-  //             city: "55",
-  //             slugs: {
-  //               restaurant: "gaagar-tt-nagar-new-market",
-  //               city: "bhopal",
-  //             },
-  //             cloudinaryImageId: "nhaax0gshegi6khog5th",
-  //             address: "32/42, New Market, TT Nagar, Bhopal",
-  //             areaName: "New Market",
-  //             costForTwo: "30000",
-  //             costForTwoMessage: "₹300 FOR TWO",
-  //             cuisines: [
-  //               "Sweets",
-  //               "Beverages",
-  //               "Desserts",
-  //               "Chaat",
-  //               "South Indian",
-  //               "North Indian",
-  //             ],
-  //             avgRating: 3.9,
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  //   {
-  //     card: {
-  //       card: {
-  //         restaurant: {
-  //           info: {
-  //             id: "80278",
-  //             name: "Gaagar New Market",
-  //             city: "55",
-  //             slugs: {
-  //               restaurant: "gaagar-tt-nagar-new-market",
-  //               city: "bhopal",
-  //             },
-  //             cloudinaryImageId: "nhaax0gshegi6khog5th",
-  //             address: "32/42, New Market, TT Nagar, Bhopal",
-  //             areaName: "New Market",
-  //             costForTwo: "30000",
-  //             costForTwoMessage: "₹300 FOR TWO",
-  //             cuisines: [
-  //               "Sweets",
-  //               "Beverages",
-  //               "Desserts",
-  //               "Chaat",
-  //               "South Indian",
-  //               "North Indian",
-  //             ],
-  //             avgRating: 3.5,
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  // ];
   const [listOfRestaurants, setListOfRestaurents] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [searchText, setSearchText] = useState("");
@@ -145,9 +19,13 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
+    // const data = await fetch(
+    //   "https://www.swiggy.com/dapi/restaurants/search/v3?lat=23.239583&lng=77.442253&str=veg&trackingId=10027316-2a7f-a2b3-24c5-b5cd52ce65cb&submitAction=ENTER&queryUniqueId=c02448cf-cb07-3b82-4e5f-866f8caa7eac"
+    // );
     const data = await fetch(
-      "/api/dapi/restaurants/search/v3?lat=23.239583&lng=77.442253&str=veg&trackingId=10027316-2a7f-a2b3-24c5-b5cd52ce65cb&submitAction=ENTER&queryUniqueId=c02448cf-cb07-3b82-4e5f-866f8caa7eac"
+      "https://portfolio-backend-oeyo.onrender.com/res-data"
     );
+
     const jsonData = await data.json();
     console.log(jsonData);
     setListOfRestaurents(
@@ -184,7 +62,7 @@ const Body = () => {
             className="px-4 py-1 mx-3 rounded-lg bg-green-200"
             onClick={() => {
               console.log(searchText);
-              
+
               const filteredRestaurent = listOfRestaurants.filter((res) =>
                 res?.card?.card?.restaurant?.info?.cuisines?.includes(
                   searchText
